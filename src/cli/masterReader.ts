270b55629f8854f57c6af7c7c7bd5928fedde0a4
Outvi V <git@outv.im>
2023-08-02 00:19:39 +0800

    feat(masterReader): use decodePayload for masterReader

--- src/cli/masterReader.ts
+++ src/cli/masterReader.ts
@@ -1,29 +1,40 @@
 import * as fs from "node:fs"
-import * as msgpack from "msgpack-lite"
-import { ExtBuffer } from "msgpack-lite/lib/ext-buffer"
+import decodePayload from "#/utils/decodePayload"
+import getClassProperties from "#/utils/getClassProperties"
+import type { ClassField } from "#/utils/getClassProperties"
 
-import lz4Decompress from "#/utils/lz4Decompress"
+function buildNameArray(structDef: ClassField[]): string[] {
+  const ret: string[] = []
+  for (const item of structDef) {
+    ret[item.index] = item.name
+  }
+  return ret
+}
 
-function decompress(data: ExtBuffer) {
-  if (data.type !== 0x63) {
-    throw Error(`Invalid LZ4Block payload - type=${data.type}`)
+function mergeDataAndStruct(
+  data: any[][],
+  structDef: ClassField[]
+): Record<string, any>[] {
+  const ret: Record<string, any>[] = []
+  for (const line of data) {
+    const v: Record<string, any> = {}
+    const nameArray = buildNameArray(structDef)
+    for (const [key, val] of Object.entries(line)) {
+      const k = nameArray[Number(key)]
+      if (k === undefined) continue
+      v[k] = val
+    }
+    ret.push(v)
   }
-  // the first 5 bytes is a "int 32" indicating the decompressed size
-  const lz4Payload = data.buffer.subarray(5)
-  const decompressedPayload = lz4Decompress(lz4Payload)
-  try {
-    return msgpack.decode(decompressedPayload)
-  } catch (e) {
-    fs.writeFileSync("debug.bin", decompressedPayload)
-    throw e
-  }
+  return ret
 }
 
 class MasterReader {
   #tree: [string, [number, number]][] | null = null
   #tableId = 0
+  #srcLines: string[] = []
 
-  init() {
+  init(srcLines: string[]) {
     if (fs.existsSync("mm/")) {
       fs.rmSync("mm/", {
         recursive: true,
@@ -31,31 +42,39 @@ class MasterReader {
       })
     }
     fs.mkdirSync("mm/")
+    this.#srcLines = srcLines
   }
-  onData(d: any) {
+  push(data: any) {
     if (this.#tree === null) {
       // the first payload is the tree
       console.debug("Reading MM tree")
-      this.#tree = d
-      fs.writeFileSync("mm/__tree.json", JSON.stringify(d, null, 2))
+      this.#tree = data
+      fs.writeFileSync("mm/__tree.json", JSON.stringify(data, null, 2))
     } else {
       const [tableName, [offset, size]] = Object.entries(this.#tree)[
         this.#tableId++
       ]
       console.debug(`Reading part: ${tableName} - expected size: ${size}`)
-      const data = d instanceof ExtBuffer ? decompress(d) : d
-      fs.writeFileSync(`mm/${tableName}.json`, JSON.stringify(data, null, 2))
+      const structDef = getClassProperties(tableName, this.#srcLines)
+      if (structDef.length === 0) {
+        console.warn(`No struct definition found for ${tableName}`)
+      }
+      console.log(tableName, data[0], structDef)
+      fs.writeFileSync(
+        `mm/${tableName}.json`,
+        JSON.stringify(mergeDataAndStruct(data, structDef), null, 2)
+      )
     }
   }
 }
 
 ;(async () => {
-  const readStream = fs.createReadStream(process.argv[2])
-  const decodeStream = msgpack.createDecodeStream()
+  const masterBuf = fs.readFileSync(process.argv[2])
+  const decodeResults = decodePayload(masterBuf)
 
   const mr = new MasterReader()
-  mr.init()
-  readStream.pipe(decodeStream).on("data", (x) => {
-    mr.onData(x)
-  })
+  mr.init(fs.readFileSync(process.argv[3], "utf-8").split("\n"))
+  for (const i of decodeResults) {
+    mr.push(i)
+  }
 })()
