30f587c67eb14596ecb2efbe0927a2e3c3c060e3
Outvi V <git@outv.im>
2023-08-05 00:00:39 +0800

    fix(convertHexLike): correctly set start/end point

--- src/utils/convertHexLike.spec.ts
+++ src/utils/convertHexLike.spec.ts
@@ -23,4 +23,14 @@ it("convertHexLike", function () {
     `68 65 6c 6c 6f 2c 20 77  6f 72 6c 64 20 7c 20 48
 45 4c 4c 4f 57 4f 52 4c  44 20 21 21`
   )
+
+  expect(
+    convertHexLike(
+      `68 65 6c 6c 6f 2c 20 77  6f 72 6c 64 20 7c 20 48
+45 4c 4c 4f 57 4f 52 4c  44 20 21 21`
+    )
+  ).to.eq(
+    `68 65 6c 6c 6f 2c 20 77  6f 72 6c 64 20 7c 20 48
+45 4c 4c 4f 57 4f 52 4c  44 20 21 21`
+  )
 })
--- src/utils/convertHexLike.ts
+++ src/utils/convertHexLike.ts
@@ -14,7 +14,9 @@ export default function convertHexLike(s: string): string {
     return s
   }
 
-  const startIndex = lineRef.index! - 1
-  const endIndex = startIndex + lineRef[0].length - 1
-  return lines.map((x) => x.slice(startIndex, endIndex + 1).trim()).join("\n")
+  const startIndex = lineRef.index!
+  const endIndex = startIndex + lineRef[0].length
+  return lines
+    .map((x) => (" " + x + " ").slice(startIndex, endIndex + 1).trim())
+    .join("\n")
 }
