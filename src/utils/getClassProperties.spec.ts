53fd8a901431b076fdf1c2772d96c03d5d55927c
Outvi V <git@outv.im>
2023-11-20 23:48:14 +0800

    tests: update imports

--- src/utils/getClassProperties.spec.ts
+++ src/utils/getClassProperties.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import getClassProperties from "./getClassProperties"
+import { getClassProperties } from "./getClassProperties"
 
 it("getClassProperties", function () {
   expect(
